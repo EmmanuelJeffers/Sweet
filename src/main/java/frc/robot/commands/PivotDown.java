c3068a2f15d484a974b9a25e63e7e71be63991fb
amina <114757533+amina7mona@users.noreply.github.com>
2023-11-14 20:19:42 -0800

    added commands for intake n pivot

--- src/main/java/frc/robot/commands/PivotDown.java
+++ src/main/java/frc/robot/commands/PivotDown.java
@@ -3,26 +3,35 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+import frc.robot.subsystems.Pivot;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Outake extends CommandBase {
-  /** Creates a new Outake. */
-  public Outake() {
+public class PivotDown extends CommandBase {
+  /** Creates a new PivotDown. */
+  private final Pivot s_Pivot;
+
+  public PivotDown(Pivot pivot) {
     // Use addRequirements() here to declare subsystem dependencies.
+  addRequirements(pivot);
+  this.s_Pivot = pivot;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    s_Pivot.pivotDown();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    s_Pivot.noPivot();
+  }
 
   // Returns true when the command should end.
   @Override
---
+++ src/main/java/frc/robot/commands/PivotUp.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import frc.robot.subsystems.Pivot;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PivotUp extends CommandBase {
+  /** Creates a new PivotUp. */
+  private final Pivot s_Pivot;
+
+  public PivotUp(Pivot pivot) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(pivot);
+    this.s_Pivot = pivot;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    s_Pivot.pivotUp();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    s_Pivot.noPivot();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
